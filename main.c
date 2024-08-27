991d56e1437255b41fd65f8eec7d79fe9253f157
ana_rchy <hmmmtesthmmm2@gmail.com>
2024-08-27 04:42:06 +0100

    fontttt

--- main.c
+++ main.c
@@ -4,12 +4,14 @@
 #define SCREEN_Y 576
 #define SCREEN_TEXT_X 128
 #define SCREEN_TEXT_Y 48
-#define UNIT_X SCREEN_X / SCREEN_TEXT_X
-#define UNIT_Y SCREEN_Y / SCREEN_TEXT_Y
+#define UNIT_X (int) (SCREEN_X / SCREEN_TEXT_X)
+#define UNIT_Y (int) (SCREEN_Y / SCREEN_TEXT_Y)
 
 int main() {
     InitWindow(SCREEN_X, SCREEN_Y, "DO NOT LOSE FOCUS.x86");
     SetTargetFPS(240);
+
+    Font font = LoadFont("font.png");
 
     char buffer[SCREEN_TEXT_X][SCREEN_TEXT_Y] = { ' ', ' ', ' ', ' ', ' ', '@' };
     
@@ -19,7 +21,7 @@ int main() {
         ClearBackground(RAYWHITE);
         for (int i = 0; i < SCREEN_TEXT_X; i++) {
             for (int j = 0; j < SCREEN_TEXT_Y; j++) {
-                DrawText(&buffer[i][j], UNIT_X * i, UNIT_Y * j, 1, BLACK);
+                DrawTextEx(font, &buffer[i][j], (Vector2) { UNIT_X * i, UNIT_Y * j }, 12, 0, BLACK);
             }
         }
 
